90b44d4f8a66f23d8b7377e43f04e9bda38498e0
Daniel Yamins <dyamins@gmail.com>
2021-01-17 15:07:11 -0800

    minor

--- models/generate_stimuli.py
+++ models/generate_stimuli.py
@@ -44,43 +44,55 @@ def main(output_dir, num, launch_build=True, port=1071):
     scenarios = get_drop_target_pairs(SCENARIOS)
     scenes = list(zip(scenarios, SCENARIO_TYPES))
     #scenes = scenes[:1]
-    for (sd, st), tp in scenes:
+    for i, ((sd, st), tp) in enumerate(scenes):
         if isinstance(sd, str):
             drop_obj = sd 
-            drop_rotation = [0, 0, 0]
+            drop_rotation = {"x": 0, "y": 0, "z": 0}
             drop_scale = 1
         else:
             drop_obj, drop_rotation, drop_scale = sd
         if isinstance(st, str):
             target_obj = st
-            target_rotation = [0, 0, 0]
+            target_rotation = {"x": 0, "y": 0, "z": 0}
             target_scale = 1
         else:
             target_obj, target_rotation, target_scale = st
 
-        dc = drop.Drop(launch_build=launch_build,
+        suffix = '%s_on_%s_%s' % (drop_obj, target_obj, tp)
+        output_path = os.path.join(output_dir, suffix)
+        temp_path = 'tmp' 
+
+        if i == 0:
+            dc = drop.Drop(launch_build=launch_build,
                        port=port,
                        randomize=0,
                        seed=0,
-                       height_range=[1.1, 1.1],
+                       height_range=[1.2, 1.2],
                        drop_scale_range=[drop_scale, drop_scale],
                        drop_jitter=0.2,
-                       drop_rotation=drop_rotation,
+                       drop_rotation_range=drop_rotation,
                        drop_objects = [drop_obj],
                        target_objects = [target_obj],
                        target_scale_range=[target_scale, target_scale],
-                       target_rotation=target_rotation,
-                       camera_radius=1.25,
+                       target_rotation_range=target_rotation,
+                       camera_radius=1.5,
                        camera_min_angle=0,
-                       camera_max_angle=0
-        )
+                       camera_max_angle=0)
 
-        suffix = '%s_on_%s_%s' % (drop_obj, target_obj, tp)
-        output_path = os.path.join(output_dir, suffix)
-        dc.run(num=num,
-               output_dir=output_path,
-               temp_path='tmp',
-               width=256,
-               height=256)
-        time.sleep(10)
+            initialization_commands = dc.get_initialization_commands(width=256,
+                                                                     height=256)
+            dc.communicate(initialization_commands)
+        else:
+            dc.drop_scale_range = [drop_scale, drop_scale]
+            dc.target_scale_range = [target_scale, target_scale]
+            dc.drop_rotation_range = drop_rotation
+            dc.target_rotation_range = target_rotation
+            dc.drop_objects = [drop_obj]
+            dc.target_objects = [dc]
+
+        dc.trial_loop(num,
+                      output_dir = output_path,
+                      temp_path = temp_path)
                        
+    dc.communicate({"$type": "terminate"})
+
