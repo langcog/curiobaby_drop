a4c377d138180aab8f57fb31ded3a01676108bb5
Daniel Yamins <dyamins@gmail.com>
2021-01-17 19:38:07 -0800

    updates

--- models/generate_stimuli.py
+++ models/generate_stimuli.py
@@ -4,47 +4,124 @@ import drop #how are we dealing with the fact that we might want to reuse contro
 
 OBJECT_INFO = {}
 
-CONTAIN_SCENARIOS = [('bowl', ['pyramid', 'trianular_prism']),
-                     ('cone', ['torus', 'octahedron']),
-                     ('dumbbell', ['pipe', 'pyramid']),
-                     ('octahedron', ['pipe', 'sphere']),
-                     ('pentagon', ['bowl', 'sphere']),
-                     ('pipe', ['cone', 'octahedron']),
-                     ('pyramid', ['torus', 'dumbbell']),
-                     ('sphere', ['bowl', 'pentagon']),
-                     ('torus', ['cone', 'dumbbell']),
-                     ('triangular_prism', ['bowl', 'dumbbell'])]
+CONTAIN_SCENARIOS = [
+                     (('bowl', {"x": 0, "y": 0, "z": 180}, 1.3), 
+                      ['pyramid', 
+                       ('triangular_prism', {"x": 0, "y": 90., "z": 0}, 1)]),
+                     (('cone', {"x": 0, "y": 0, "z": 180}, 0.75),
+                      ['torus', 
+                      ('octahedron', {"x": 0, "y": 0, "z": 90}, 1)]),
+                     (('dumbbell', {"x": 90, "y": 0, "z": 0}, .4), 
+                      [('pipe', 
+                        {"x": 0, "y": 0, "z": 0}, 
+                        {"x": 0.9, "y": 0.315, "z": 0.9}),
+                       ('pyramid', 
+                        {"x": 0, "y": 0, "z": 0}, 
+                        0.8)]),
+                     ('octahedron', 
+                      [('pipe', 
+                        {"x": 0, "y": 0, "z": 0}, 
+                        {"x": 0.9, "y": 0.315, "z": 0.9}),
+                      'sphere']),
+                     (('pentagon',
+                       {"x": 0, "y": 0, "z": 0}, 
+                       {"x": 0.7, "y": 0.49, "z": 0.7}), 
+                      ['bowl', 
+                       ('sphere',{"x": 0, "y": 0, "z": 0}, 0.7)]),
+                     (('pipe', 
+                        {"x": 0, "y": 0, "z": 0}, 
+                        {"x": 1, "y": 0.35, "z": 1}),
+                      ['cone', 
+                      ('octahedron', {"x": 0, "y": 0, "z": 90.}, 1)]),
+                     (('pyramid', {"x": 0, "y": 0, "z": 180}, 1), 
+                      ['torus', ('dumbbell', {"x": 0, "y": 0, "z": 0}, 0.6)]),
+                     (('sphere', {"x": 0, "y": 0, "z": 0}, 0.8), 
+                      ['bowl', 
+                       ('pentagon',
+                        {"x": 0, "y": 0, "z": 0}, 
+                        {"x": 0.8, "y": 0.56, "z": 0.8})
+                       ]),
+                     (('torus', {"x": 0, "y": 0, "z": 0}, 5), 
+                      [('cone', {"x": 0, "y": 0, "z": 0}, 0.4), 
+                       'dumbbell']),
+                     (('triangular_prism',
+                       {"x": 0, "y": 0, "z": 90},
+                       {"x": 1, "y": 0.6, "z": 0.6}),
+                      ['bowl', 
+                       ('dumbbell', {"x": 0, "y": 0, "z": 0}, 0.5)])
+                     ]
 
-SUPPORT_SCENARIOS = [('bowl', ['torus', 'triangular_prism']),
-                     ('cone', ['triangular_prism', 'sphere']),
-                     ('dumbbell', ['pentagon', 'octahedron']),
-                     ('octahedron', ['pentagon', 'pyramid']),
-                     ('pentagon', ['pipe', 'pyramid']),
-                     ('pipe', ['torus', 'dumbbell']),
-                     ('pyramid', ['triangular_prism', 'sphere']),
-                     ('sphere', ['pipe', 'cone']),
-                     ('torus', ['bowl', 'octahedron']),
-                     ('triangular_prism', ['pentagon', 'cone'])]
+SUPPORT_SCENARIOS = [
+                     (('bowl', {"x": 0, "y": 0, "z": 180}, 1), 
+                      [('torus', {"x": 0, "y": 0, "z": 0}, 0.8), 
+                       ('triangular_prism', 
+                        {"x": 0, "y": 0, "z": 0},
+                        {"x": 1, "y": 0.6, "z": 0.6})]),
+                     ('cone', 
+                      [('triangular_prism',
+                        {"x": 0, "y": 0., "z": 90}, 
+                        {"x": 1, "y": 0.8, "z": 0.8}),
+                       'sphere']),
+                     (('dumbbell', {"x": 0, "y": 0, "z": 0}, 0.7), 
+                      [('pentagon', 
+                        {"x": 0, "y": 0, "z": 0}, 
+                        {"x": 1, "y": 0.7, "z": 1}),
+                      'octahedron']),
+                     (('octahedron', {"x": 0, "y": 0, "z": 90.}, 1), 
+                     [('pentagon', 
+                        {"x": 0, "y": 0, "z": 0}, 
+                        {"x": 1, "y": 0.7, "z": 1}), 
+                      'pyramid']),
+                     (('pentagon', 
+                      {"x": 0, "y": 0, "z": 0},
+                      {"x": 1, "y": 0.7, "z": 1}), 
+                     [('pipe', 
+                       {"x": 0, "y": 0, "z": 0}, 
+                       {"x": 1, "y": 0.35, "z": 1}),
+                      'pyramid']),
+                     (('pipe',
+                      {"x": 0, "y": 0, "z": 0},
+                      {"x": 1.1, "y": 0.35, "z": 1.1}), 
+                     ['torus', 
+                     ('dumbbell', {"x": 0, "y": 0, "z": 0}, 0.5)]),
+                     (('pyramid', {"x": 0, "y": 0., "z": 0}, .8), 
+                      [('triangular_prism', 
+                       {"x": 0, "y": 0., "z": 90}, 
+                       {"x": 1.2, "y": 0.96, "z": 0.96}),
+                       'sphere']),
+                     ('sphere', 
+                      [('pipe',
+                        {"x": 0, "y": 0, "z": 0},
+                        {"x": 1.1, "y": 0.35, "z": 1.1}),
+                      'cone']),
+                     (('torus', {"x": 0, "y": 0, "z": 0}, 4), 
+                      ['bowl', 
+                       ('octahedron', {"x": 0, "y": 0, "z": 0}, 0.4)]),
+                     ('triangular_prism', 
+                      [('pentagon',
+                        {"x": 0, "y": 0, "z": 0},
+                        {"x": 1, "y": 0.7, "z": 1}),
+                      'cone'])
+                     ]
 
-SCENARIO_TYPES = 10 * ['contain'] + 10 * ['support']
+CONTAIN_SCENARIOS = [(x, 'contain') for x in CONTAIN_SCENARIOS]
+SUPPORT_SCENARIOS = [(x, 'support') for x in SUPPORT_SCENARIOS]
 
 SCENARIOS = CONTAIN_SCENARIOS + SUPPORT_SCENARIOS
 
 def get_drop_target_pairs(scenarios):
     pairs = []
-    for sd, st in scenarios:
-        if (sd, st[0]) not in scenarios:
-            pairs.append((sd, st[0]))
-        if (sd, st[1]) not in scenarios:
-            pairs.append((sd, st[1]))
+    for (sd, st), stype in scenarios:
+        if ((sd, st[0]), stype) not in scenarios:
+            pairs.append(((sd, st[0]), stype))
+        if ((sd, st[1]), stype) not in scenarios:
+            pairs.append(((sd, st[1]), stype))
     return pairs
 
 
 def main(output_dir, num, launch_build=True, port=1071):
     scenarios = get_drop_target_pairs(SCENARIOS)
-    scenes = list(zip(scenarios, SCENARIO_TYPES))
-    #scenes = scenes[:1]
-    for i, ((sd, st), tp) in enumerate(scenes):
+    for i, ((sd, st), tp) in enumerate(scenarios):
         if isinstance(sd, str):
             drop_obj = sd 
             drop_rotation = {"x": 0, "y": 0, "z": 0}
@@ -67,13 +144,13 @@ def main(output_dir, num, launch_build=True, port=1071):
                        port=port,
                        randomize=0,
                        seed=0,
-                       height_range=[1.2, 1.2],
-                       drop_scale_range=[drop_scale, drop_scale],
-                       drop_jitter=0.2,
+                       height_range=[1.9, 1.9],
+                       drop_scale_range=drop_scale,
+                       drop_jitter=0.1,
                        drop_rotation_range=drop_rotation,
                        drop_objects = [drop_obj],
                        target_objects = [target_obj],
-                       target_scale_range=[target_scale, target_scale],
+                       target_scale_range=target_scale,
                        target_rotation_range=target_rotation,
                        camera_radius=1.5,
                        camera_min_angle=0,
@@ -83,12 +160,13 @@ def main(output_dir, num, launch_build=True, port=1071):
                                                                      height=256)
             dc.communicate(initialization_commands)
         else:
-            dc.drop_scale_range = [drop_scale, drop_scale]
-            dc.target_scale_range = [target_scale, target_scale]
+            dc.clear_static_data()
+            dc.drop_scale_range = drop_scale
+            dc.target_scale_range = target_scale
             dc.drop_rotation_range = drop_rotation
             dc.target_rotation_range = target_rotation
-            dc.drop_objects = [drop_obj]
-            dc.target_objects = [dc]
+            dc.set_drop_types([drop_obj])
+            dc.set_target_types([target_obj])
 
         dc.trial_loop(num,
                       output_dir = output_path,
