2c09c515c185748cafd05390ff2e4e0729f46465
Daniel Yamins <dyamins@gmail.com>
2021-01-19 23:10:01 -0800

    additional models

--- models/model.py
+++ models/model.py
@@ -1,6 +1,4 @@
-from frozendict import frozendict
-
-import numpy as numpy
+import numpy as np
 
 
 ################
@@ -17,8 +15,12 @@ class CuriosityBinaryChoice(object):
 	def __init__(self,
 			     curiosity_function,
 			     linking_function=softmax,
-			     curiosity_kwargs=frozendict({}),
-			     linking_kwargs=frozendict({})):
+			     curiosity_kwargs=None,
+			     linking_kwargs=None):
+		if curiosity_kwargs is None:
+			curiosity_kwargs = {}
+		if linking_kwargs is None:
+			lining_kwargs = {}
 		self.curiosity_function = curiosity_function
 		self.linking_function = linking_function
 		self.curiosity_kwargs = curiosity_kwargs
@@ -26,8 +28,8 @@ class CuriosityBinaryChoice(object):
 
 	def judgement(self, data):
 		data_left, data_right = data
-		m0 = self.curiosity(data_left, **self.curiosity_kwargs)
-		m1 = self.curiosity(data_right, **self.curiosity_kwargs)
+		m0 = self.curiosity_function(data_left, **self.curiosity_kwargs)
+		m1 = self.curiosity_function(data_right, **self.curiosity_kwargs)
 		judgement = self.linking_function([m0, m1], **self.linking_kwargs)
 		return {'curiosity_left': m0,
 			    'curiosity_right': m1,
@@ -106,13 +108,41 @@ def get_positions_from_frames(d, object_inds=None):
 	returns array of shape (num_frames, num_objects_in_selection, 3)
 	"""
 	pos = []
-	for f in d['frames'].values():
-		p = _f['objects']['positions'][:]
+	frame_inds = list(d['frames'].keys())
+	frame_inds.sort()
+	for f_ind in frame_inds:
+		f = d['frames'][f_ind]
+		p = f['objects']['positions'][:]
 		if object_inds is not None:
 			p = p[:, object_inds]
 		pos.append(p)
 	return np.array(pos)
 
+
+def get_velocities(data, object_inds=None):
+	"""
+	helper function getting all positions of all frames in all trials
+	returns a list rather than an array since the len of the trials will be different
+	"""
+	posfunc = lambda x: get_velocities_from_frames(x,
+						     object_inds=object_inds)
+	return list(map(posfunc, data))
+
+
+def get_velocities_from_frames(d, object_inds=None):
+	"""helper function getting velocities for all frames in a trial
+	returns array of shape (num_frames, num_objects_in_selection, 3)
+	"""
+	vel = []
+	frame_inds = list(d['frames'].keys())
+	frame_inds.sort()
+	for f_ind in frame_inds:
+		f = d['frames'][f_ind]
+		v = f['objects']['velocities'][:]
+		if object_inds is not None:
+			v = v[object_inds]
+		vel.append(v)
+	return np.array(vel)
 
 def get_object_inds(objects):
 	#XXX TODO relies on fact that target is placed first...
@@ -133,9 +163,9 @@ def obj_final_position_std(data, objects=None):
 	"""
 	object_inds = get_object_inds(objects)
 	posvecs = get_last_frame_positions(data)
-	if object_inds is not None
-		posvecs = posvecs[:, object_inds]
-	total_std = posvecs.std(axis=0).sum(axis=0).sum(axis=0)
+	if object_inds is not None:
+		posvecs = posvecs[object_inds]
+	total_std = np.sqrt(posvecs.var(axis=0).sum(axis=0).sum(axis=0))
 	return total_std
 
 
@@ -180,3 +210,30 @@ def max_radius_std(data, objects=None):
 ####################
 #####COLLISIONS#####
 ####################
+
+def get_first_collision_frame(d):
+	collision_frames = get_collision_frames(d)
+	collision_frames.sort()
+	if len(collision_frames) > 0:
+		return collision_frames[0]
+	else:
+		return None
+
+
+def get_collision_frames(d):
+	f_inds = list(d['frames'].keys())
+	f_inds.sort()
+	collision_frames = np.nonzero([d['frames'][fn]['collisions']['contacts'].shape[0] for fn in f_inds])[0]
+	return collision_frames
+
+
+def velocity_std_after_first_collision(data, objects=None, window=5):
+    fcfs = list(map(get_first_collision_frame, data))
+    object_inds = get_object_inds(objects)
+    vels = get_velocities(data, object_inds=object_inds)
+    #V is of shape (num_trials, window, num_objects, 3)
+    V = np.array([v[fcf:fcf + window] for v, fcf in zip(vels, fcfs) if fcf is not None])
+    Vm = V.mean(axis=1)
+    return np.sqrt(Vm.var(axis=0).sum())
+
+
